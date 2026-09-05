09b79297e6b7f5b43dc17d4041fc40445a58882b
Kaique Eduardo Costa Paiva <duducpaiva@gmail.com>
2026-09-05 00:34:38 -0200

    Implement motor function in prob1.c
    Add motor function to find the first decreasing position in an array.

--- função/prob1.c
+++ função/prob1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+void motor(int v[],int qntd)
+{
+    int i;
+    int posicao;
+    int valor = 0;
+    
+    for(i=1;i<qntd;i++)
+    {
+        posicao = 0;
+        if(v[i]<v[i - 1])
+        {
+            posicao = i + 1;
+            break;
+        }
+    }
+    printf("%d\n",posicao);
+}
+
+int main()
+{
+    int qntd,i;
+    scanf("%d",&qntd);
+    
+    int v[qntd];
+    for(i=0;i<qntd;i++)
+    {
+        scanf("%d",&v[i]);
+    }
+
+    motor(v,qntd);
+}
