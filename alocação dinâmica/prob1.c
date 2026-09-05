5ea8ddbe34c80a53c1ca13999402c7833a67ffa8
Kaique Eduardo Costa Paiva <duducpaiva@gmail.com>
2026-09-05 00:40:17 -0200

    Fix formatting and structure in prob1.c

--- alocação dinâmica/prob1.c
+++ alocação dinâmica/prob1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main() 
+{
     int n, i;
     int *vetor;
 
@@ -9,11 +10,13 @@ int main() {
 
     vetor = (int *)malloc(n * sizeof(int));
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         scanf("%d", &vetor[i]);
     }
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         printf("%d ", vetor[i]);
     }
     printf("\n");
