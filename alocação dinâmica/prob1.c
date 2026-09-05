aead50e8b663a6fd898f0348f2c9ba1c7f557c9b
Kaique Eduardo Costa Paiva <duducpaiva@gmail.com>
2026-09-05 00:32:12 -0200

    Implement dynamic array allocation in prob1.c
    Add dynamic memory allocation for an integer array.

--- alocação dinâmica/prob1.c
+++ alocação dinâmica/prob1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int n, i;
+    int *vetor;
+
+    scanf("%d", &n);
+
+    vetor = (int *)malloc(n * sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    free(vetor);
+}
