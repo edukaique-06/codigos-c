9fb77443cd9e38a95e3529266006c89347d855c8
Kaique Eduardo Costa Paiva <duducpaiva@gmail.com>
2026-09-05 00:39:10 -0200

    Refactor formatting and structure of prob1.c

--- recursividade/prob1.c
+++ recursividade/prob1.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-void ordenar(int *v, int n) {
+void ordenar(int *v, int n) 
+{
     int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (v[j] > v[j + 1]) {
+    for (i = 0; i < n - 1; i++) 
+    {
+        for (j = 0; j < n - i - 1; j++)
+            {
+            if (v[j] > v[j + 1]) 
+            {
                 temp = v[j];
                 v[j] = v[j + 1];
                 v[j + 1] = temp;
@@ -13,31 +17,37 @@ void ordenar(int *v, int n) {
     }
 }
 
-int busca_binaria_rec(int *v, int inicio, int fim, int chave) {
-    if (inicio > fim) {
+int busca_binaria_rec(int *v, int inicio, int fim, int chave) 
+{
+    if (inicio > fim) 
+    {
         return -1;
     }
 
     int meio = inicio + (fim - inicio) / 2;
 
-    if (v[meio] == chave) {
+    if (v[meio] == chave) 
+    {
         return meio;
     }
 
-    if (chave < v[meio]) {
+    if (chave < v[meio]) 
+    {
         return busca_binaria_rec(v, inicio, meio - 1, chave);
     }
 
     return busca_binaria_rec(v, meio + 1, fim, chave);
 }
 
-int main() {
+int main()
+{
     int n, chave, i;
     int vetor[100];
 
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         scanf("%d", &vetor[i]);
     }
 
