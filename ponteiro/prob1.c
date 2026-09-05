ab52bb8248dc954002391fac19a841b9046fac83
Kaique Eduardo Costa Paiva <duducpaiva@gmail.com>
2026-09-05 00:33:04 -0200

    Implement swap function for two integers

--- ponteiro/prob1.c
+++ ponteiro/prob1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+void trocar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    scanf("%d %d", &x, &y);
+
+    trocar(&x, &y);
+
+    printf("%d %d\n", x, y);
+}
